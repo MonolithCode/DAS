d23db7881010e69135495f716a2680d194638105
= <=>
2015-05-10 13:43:59 +0100

    first commit

---
+++ WebApplication4/App_Code/OptimisePress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication4.LunchboxAPI;
+
+namespace WebApplication4
+{
+    public static class OptimisePress
+    {
+
+
+        public static bool LoginWp(string username, string password)
+        {
+            var api = new DomainAuctionSniperAPI();
+            if ((api.LoginWP(username, password) == "MATCHED"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
